5dd92b79e8a8ec5f13aa7e337cb618380e62207e
asd <114643897+Lovescream@users.noreply.github.com>
2024-07-30 18:30:02 +0900

    GPGS Setup

--- Blossom/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Blossom/Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "522150525161"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "522150525161-tbb67ummdo0q7k702ljhuo9fmv8nucdm.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
